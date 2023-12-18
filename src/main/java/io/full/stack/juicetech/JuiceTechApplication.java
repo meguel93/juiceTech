645fd5d6e0b3d49259626ef8a5c215b40db54460
pfano <meguel93@gmail.com>
2023-12-18 21:00:25 +0200

    init commit

---
+++ src/main/java/io/full/stack/juicetech/JuiceTechApplication.java
@@ -0,0 +1,13 @@
+package io.full.stack.juicetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuiceTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuiceTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/full/stack/juicetech/JuiceTechApplicationTests.java
@@ -0,0 +1,13 @@
+package io.full.stack.juicetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
